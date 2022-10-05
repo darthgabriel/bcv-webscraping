bbc408b51551c3e74108fa20fd2f12d269de6b6b
Jose Paredes - Trabajo <joseparedes@ocg.net.ve>
2022-10-05 09:18:36 -0400

    readme y method post en api

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,25 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const axios = require('axios');
 const cheerio = require('cheerio');
-const https = require("https");
+const https = require('https');
 
 const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
+	rejectUnauthorized: false,
 });
 
 export default async function handler(req, res) {
-  
+	if (req.method === 'POST') {
+		const tasa = await axios
+			.get('http://www.bcv.org.ve/', { httpsAgent })
+			.then((res) => {
+				const $ = cheerio.load(res.data);
+				const string = $('div#dolar').text();
+				let limpio = string.replace(/\s+/g, '');
+				limpio = limpio.replace('USD', '');
+				limpio = limpio.replace(',', '.');
+				limpio = Number(limpio);
+				return limpio;
+			})
+			.catch((err) => console.error(err));
 
-  const tasa = await axios.get('http://www.bcv.org.ve/',{ httpsAgent })
-  .then(res => {
-      const $ = cheerio.load(res.data)
-      const string = $('div#dolar').text()
-      let limpio = string.replace(/\s+/g, '')
-      limpio = limpio.replace('USD', '')
-      limpio = limpio.replace(',', '.')
-      limpio = Number(limpio)
-      return limpio;
-  }).catch(err => console.error(err))
-
-  res.status(200).json({ tasa })
+		res.status(201).json({ tasa });
+	} else {
+		res.status(401).end();
+	}
 }
