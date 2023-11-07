af9bec9f24a4e206e480fe153a45d41a16c2b438
Jose Casa <gabriel.changed@gmail.com>
2023-11-07 13:54:04 -0400

    fix errores

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,53 +1,49 @@
-import { useState, useEffect, useRef } from 'react';
-import Head from 'next/head';
-import axios from 'axios';
-import Image from 'next/image';
-import moment from 'moment';
-import 'moment/locale/es';
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import axios from 'axios'
+import Image from 'next/image'
+import moment from 'moment'
+import 'moment/locale/es'
 
 import Conversor from '../components/Conversor'
 
-export default function Home() {
-
+export default function Home () {
   const getTasaBcv = () => {
     axios
       .post('/api')
       .then((r) => setTasa(r.data.tasa))
-      .catch((e) => 'SIN CONEXION');
-  };
+      .catch((e) => 'SIN CONEXION')
+  }
 
-  
-	const [tasa, setTasa] = useState(getTasaBcv());
-	const [fecha, setFecha] = useState(moment().format('dddd DD [de] MMMM [del] YYYY h:mm a'));
+  const [tasa, setTasa] = useState(getTasaBcv())
+  const [fecha, setFecha] = useState(moment().format('dddd DD [de] MMMM [del] YYYY h:mm a'))
 
-	useEffect(() => {
-		setInterval(() => {
-			getTasaBcv();
-			setFecha(moment().format('dddd DD [de] MMMM [del] YYYY h:mm a'));
-		}, 3600000);
-	}, []);
+  useEffect(() => {
+    setInterval(() => {
+      getTasaBcv()
+      setFecha(moment().format('dddd DD [de] MMMM [del] YYYY h:mm a'))
+    }, 3600000)
+  }, [])
 
-	return (
-		
-		<>
-			<Head>
-				<title>BCV - Webscraping</title>
-				<meta name='description' content='BCV WebScraping' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+  return (
 
-			<main>
-				<Image width={200} height={200} objectFit='cover' src={'/bcvlogo.png'} alt='' />
-				<h1>$ USD <br /> {tasa}</h1>
-				 {tasa > 0 && <Conversor tasa={tasa} />}
-				<h4>{fecha}</h4>
-			</main>
+    <>
+      <Head>
+        <title>BCV - Webscraping</title>
+        <meta name='description' content='BCV WebScraping' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-			<footer>
-				Powered by Ing. Jose Paredes
-			</footer>
-		</>
-	);
+      <main>
+        <Image width={200} height={200} objectFit='cover' src='/bcvlogo.png' alt='' />
+        <h1>$ USD <br /> {tasa}</h1>
+        {tasa > 0 && <Conversor tasa={tasa} />}
+        <h4>{fecha}</h4>
+      </main>
+
+      <footer>
+        Powered by Ing. Jose Paredes
+      </footer>
+    </>
+  )
 }
-
-
